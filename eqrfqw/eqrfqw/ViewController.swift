7902930df3e672ce529bd0f27c4e1985dd371aff
rajnikant <rajnikant@Rajanikant.local>
2018-08-06 12:25:45 +0530

    first commit

---
+++ eqrfqw/eqrfqw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eqrfqw
+//
+//  Created by rajnikant on 06/08/18.
+//  Copyright © 2018 rajnikant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
